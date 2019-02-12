058bb099e4ca80c14a7de5e976bd323ad01af049
abhijithshankar93 <abhijithshankar.89@gmail.com>
2019-02-12 10:39:31 +0530

    Minor changes

--- Reference/graph_bfs.py
+++ Reference/graph_bfs.py
@@ -1,4 +1,17 @@
 # Depth First Search of a Tree
+'''
+Refer: https://www.tutorialspoint.com/python/python_graph_algorithms.htm
+
+Also called depth first search (DFS),this algorithm traverses a graph in 
+a depth ward motion and uses a stack to remember to get the next vertex to 
+start a search, when a dead end occurs in any iteration. We implement DFS 
+for a graph in python using the set data types as they provide the required 
+functionalities to keep track of visited and unvisited nodes.
+
+
+
+'''
+
 
 class Graph:
 	def __init__(self, graph_dict=None):
