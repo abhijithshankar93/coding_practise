9f936e57c81af07527ada3c95d2ce647e230d1b0
abhijithshankar93 <abhijithshankar.89@gmail.com>
2019-02-17 00:54:52 +0530

    olution to day5 of DailyCoding Problem

---
+++ DailyCodingProblem/day5.py
@@ -0,0 +1,44 @@
+'''
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first 
+and last element of that pair. For example, car(cons(3, 4)) returns 3, and 
+cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+'''
+
+
+def cons(a, b):
+	def pair(f):
+		return f(a, b)
+	return pair
+
+
+def car(f):
+	def left(a,b):
+		return a
+	return f(left)
+
+def cdr(f):
+	def right(a,b):
+		return b
+	return f(right)
+
+
+if __name__ == '__main__':
+
+	assert car(cons(3, 4)) == 3, 'Result returned wrong. {}'.format(car(
+																	cons(3,4))
+																	)
+	assert cdr(cons(3, 4)) ==4, 'Result returned wrong. {}'.format(cdr(
+																	cons(3,4))
+																	)
